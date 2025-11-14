af6d6f10cd2cda25a11a50ebf39161b897f7e1f7
한진희 <0200134hjh@gmail.com>
2025-11-14 20:12:36 +0900

    Update AST.hs

--- src/AST.hs
+++ src/AST.hs
@@ -2,9 +2,12 @@ module AST where
 
 data Expr
   = Number Int
+  | StringLit String
   | Var String
+  | Assign String Expr
   | Add Expr Expr
   | Sub Expr Expr
   | Mul Expr Expr
   | Div Expr Expr
+  | Seq [Expr]
   deriving (Show, Eq)

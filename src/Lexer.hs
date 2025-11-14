0f01c5d8081fa527d94ee0a81670eb45408246ee
한진희 <0200134hjh@gmail.com>
2025-11-14 20:12:59 +0900

    Update Lexer.hs

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -3,12 +3,18 @@ module Lexer where
 import Data.Char (isAlpha, isAlphaNum, isDigit)
 
 data Token
-  = TokNumber Int
+  = TokLet
   | TokIdent String
+  | TokNumber Int
+  | TokString String
+  | TokAssign
   | TokPlus
   | TokMinus
   | TokStar
   | TokSlash
+  | TokLParen
+  | TokRParen
+  | TokSemi
   | TokEOF
   deriving (Show, Eq)
 
@@ -17,15 +23,25 @@ tokenize [] = [TokEOF]
 tokenize (c:cs)
   | isAlpha c =
       let (name, rest) = span isAlphaNum (c:cs)
-      in TokIdent name : tokenize rest      
+      in case name of
+           "let" -> TokLet : tokenize rest
+           _     -> TokIdent name : tokenize rest
 
   | isDigit c =
       let (digits, rest) = span isDigit (c:cs)
       in TokNumber (read digits) : tokenize rest
 
-  | c == '+' = TokPlus  : tokenize cs
-  | c == '-' = TokMinus : tokenize cs
-  | c == '*' = TokStar  : tokenize cs
-  | c == '/' = TokSlash : tokenize cs
+  | c == '"' =
+      let (str, rest) = span (/= '"') cs
+      in TokString str : tokenize (drop 1 rest)
+
+  | c == '=' = TokAssign : tokenize cs
+  | c == '+' = TokPlus   : tokenize cs
+  | c == '-' = TokMinus  : tokenize cs
+  | c == '*' = TokStar   : tokenize cs
+  | c == '/' = TokSlash  : tokenize cs
+  | c == '(' = TokLParen : tokenize cs
+  | c == ')' = TokRParen : tokenize cs
+  | c == ';' = TokSemi   : tokenize cs
 
   | otherwise = tokenize cs
